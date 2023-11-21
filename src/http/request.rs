a13c07152fd44bed447f787a3a184d7bafb0cce1
Anthony Castellano <tonycastellano13@gmail.com>
2023-11-20 20:56:54 -0500

    add lifetime specifiers and create query string struct

--- src/http/request.rs
+++ src/http/request.rs
@@ -5,41 +5,26 @@ use std::error::Error;
 use std::fmt::{Display, Debug, Formatter, Result as FmtResult};
 use std::str;
 
-pub struct Request {
-    path: String,
-    query_method: Option<String>,
+pub struct Request<'buf> { // 'buf is lifetime
+    path: &'buf str,
+    query_string: Option<&'buf str>,
     method: Method,
 }
 
-impl Request {
-    fn from_byte_array(buf: &[u8]) -> Result<Self, Self::Error> {
+// impl Request {
+//     fn from_byte_array(buf: &[u8]) -> Result<Self, Self::Error> {
 
-    }
-}
+//     }
+// }
 
-impl TryFrom<&[u8]> for Request {
+impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
     type Error = ParseError;
 
-    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
-        // match str::from_utf8(buf) {
-        //     Ok(request) => {}
-        //     Err(_) => return Err(ParseError::InvalidEncoding)
-        // }
-
-        // match str::from_utf8(buf).or(Err(ParseError::InvalidEncoding)) {
-        //     Ok(request) => {}
-        //     Err(e) => return Err(e),
-        // }
-
+    fn try_from(buf: &'buf [u8]) -> Result<Request<'buf>, Self::Error> {
         let request = str::from_utf8(buf)?;
 
-        // match get_next_word(request) {
-        //     Some((method, request)) => {},
-        //     None => return Err(ParseError::InvalidRequest)
-        // }
-
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
-        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
 
         if protocol != "HTTP/1.1" {
@@ -47,6 +32,18 @@ impl TryFrom<&[u8]> for Request {
         }
 
         let method: Method = method.parse()?;
+
+        let mut query_string = None;
+        if let Some(i) = path.find('?') {
+            query_string = Some(&path[i + 1..]); // this means + 1 byte, not character
+            path = &path[..i];
+        }
+
+        Ok(Self {
+            path,
+            query_string,
+            method,
+        })
     }
 }
 
